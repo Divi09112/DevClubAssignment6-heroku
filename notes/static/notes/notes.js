f1bf26a07e1234b5e9958c936829d889af14666b
Divyansh <divi09112@gmail.com>
2017-12-25 12:02:43 +0530

    further styling

--- notes/static/notes/notes.js
+++ notes/static/notes/notes.js
@@ -1,45 +1,4 @@
-var k = 0;
-
-var dark = true;
-
-function enter(elem,e){
-  if (e.keyCode==13){
-    var li = document.createElement("li");
-    li.innerHTML=document.getElementById("entry").value;
-    li.setAttribute("Id", "task"+k);
-    document.getElementById("list").appendChild(li);
-    document.getElementById("entry").value='';
-    
-    if (dark){
-      li.className="dark";
-      dark = false;
-    }
-    else{
-      li.className="light";
-      dark = true;
-    };
-    
-    li.setAttribute('onClick',`strike(${k})`);
-    k++;
-  };
+console.log("9641346");
+function show(link){
+window.location.assign(link);
 }
-
-
-function strike(n){
-  var el = document.getElementById("task"+n);
-
-  if (el.className !== "clicked"){
-    el.className="clicked";
-  }
-  else{
-    const val = el.id.slice(4,el.id.length);
-    if (val%2===0) {
-      el.className = "dark";
-    }
-    else{
-      el.className = "light";
-    };
-  };
-}
-
-console.log(document.getElementById('entry').value)
